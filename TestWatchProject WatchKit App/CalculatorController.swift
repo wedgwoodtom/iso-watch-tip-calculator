ce373fd8b1c6924e2c77736860c7ec544c5ca0cb
tompatterson <wedgwood.tom@gmail.com>
2016-12-24 01:08:05 -0800

    Initial checkin of functional watch

---
+++ TestWatchProject WatchKit App/CalculatorController.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorController.swift
+//  TestWatchProject
+//
+//  Created by Tom on 12/23/16.
+//  Copyright © 2016 Tom Patterson. All rights reserved.
+//
+
+import Foundation
--- TestWatchProject WatchKit Extension/InterfaceController.swift
+++ TestWatchProject WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,24 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var amountButton: WKInterfaceButton!
+    
+    @IBOutlet var tipSlider: WKInterfaceSlider!
+    
+    @IBOutlet var totalLabel: WKInterfaceLabel!
+    
+    @IBOutlet var tipLabel: WKInterfaceLabel!
+    
+    var mealCost: Float?
+    var tipPercent: Float = 15.0
+    
+
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+//        tipSlider.setValue(tipPercent)
+//        updateFinalCost()
     }
     
     override func willActivate() {
@@ -28,4 +42,43 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    @IBAction func btnClicked() {
+        var newPrice:String?
+        
+        presentTextInputController(withSuggestions: ["57.25"], allowedInputMode: .plain,
+            completion: { (price) -> Void in
+                if (price != nil) {
+                    newPrice = price?[0] as? String
+                }
+                if (newPrice == nil) {
+                    newPrice = "0.00"
+                }
+                self.mealCost = Float(newPrice!)
+                if (self.mealCost != nil) {
+                    self.amountButton.setTitle("Bill $ " + String(format: "%.2f", self.mealCost!))
+                }
+                self.updateFinalCost()
+        })
+    }
+    
+    @IBAction func sliderChanged(_ value: Float) {
+        tipPercent = value
+        tipLabel.setText( "with \(Int(tipPercent))% tip")
+        updateFinalCost()
+    }
+    
+    func updateFinalCost() {
+        guard mealCost != nil else {
+            totalLabel.setText("$ 0.00")
+            amountButton.setTitle("Enter Bill Amount")
+            return
+        }
+        
+        totalLabel.setText("$ " + String(format: "%.2f", finalCost()))
+    }
+    
+    func finalCost() -> Float {
+        return mealCost! + tipPercent / 100.0 * mealCost!
+    }
+    
 }
